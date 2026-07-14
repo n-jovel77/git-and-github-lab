605ace0e414cb4bd9441fcd0fc91338a7dcb1a9a
Nisa Jovel <nice4056@colorado.edu>
2026-07-14 11:41:13 -0600

    Updated the README badge

--- analysis.py
+++ analysis.py
@@ -1,6 +1,12 @@
-import pandas as pd
-df = pd.read_csv("data.csv")
-print("Number of rows", len(df))
-print("mean sale amount", df["amount"].mean())
-print("Largest sale amount:", df['amount'].max())
+import csv
+from statistics import mean
+
+with open("data.csv", newline="") as csvfile:
+    reader = csv.DictReader(csvfile)
+    rows = [row for row in reader]
+
+amounts = [float(row["amount"]) for row in rows]
+print("Number of rows", len(rows))
+print("mean sale amount", mean(amounts))
+print("Largest sale amount:", max(amounts))
     
